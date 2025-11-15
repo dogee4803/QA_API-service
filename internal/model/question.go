0112bdcb6b3a68537747e0aa1dc3e5c0495548ed
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-11-15 12:06:49 +0300

    feat: Add models and conf

---
+++ internal/model/question.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"time"
+)
+
+type Question struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Text      string    `gorm:"type:text;not null" json:"text"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	
+	Answers   []Answer  `gorm:"foreignKey:QuestionID;constraint:OnDelete:CASCADE" json:"answers,omitempty"`
+}
